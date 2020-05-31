364842b813543196fd6b165108020f5455555743
bnoazx005 <ildar2571@yandex.ru>
2020-05-31 20:36:50 +0400

    Add argparse library

---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	// \todo Parse options
+	// \todo Scan given directory for cpp header files
+	// \todo Run for each header parser utility
+	// \todo Generate meta-information as cpp files
+
+	return 0;
+}
