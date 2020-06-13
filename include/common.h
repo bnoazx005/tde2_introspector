18a25811870764ff46ecdbb701474d0456583603
bnoazx005 <ildar2571@yandex.ru>
2020-06-13 20:03:44 +0400

    Add implementation of helper string functions like Format

--- include/common.h
+++ include/common.h
@@ -4,6 +4,9 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <array>
+#include <cstdint>
+#include <sstream>
 
 
 namespace TDEngine2
@@ -85,5 +88,59 @@ namespace TDEngine2
 			std::string mFilename;
 
 			std::ofstream mFileStream;
+	};
+
+
+	class StringUtils
+	{
+		public:
+			template <typename... TArgs>
+			static std::string Format(const std::string& formatStr, TArgs&&... args)
+			{
+				constexpr uint32_t argsCount = sizeof...(args);
+
+				std::array<std::string, argsCount> arguments;
+				_convertToStringsArray<sizeof...(args), TArgs...>(arguments, std::forward<TArgs>(args)...);
+
+				std::string formattedStr = formatStr;
+				std::string currArgValue;
+				std::string currArgPattern;
+
+				currArgPattern.reserve(5);
+
+				std::string::size_type pos = 0;
+
+				/// \note replace the following patterns {i}
+				for (uint32_t i = 0; i < argsCount; ++i)
+				{
+					currArgPattern = "{" + ToString<uint32_t>(i) + "}";
+					currArgValue = arguments[i];
+
+					while ((pos = formattedStr.find(currArgPattern)) != std::string::npos)
+					{
+						formattedStr.replace(pos, currArgPattern.length(), currArgValue);
+					}
+				}
+
+				return formattedStr;
+			}
+
+			template <typename T>
+			static std::string ToString(const T& arg)
+			{
+				std::ostringstream stream;
+				stream << arg;
+				return stream.str();
+			}
+	private:
+		template <uint32_t size>
+		static void _convertToStringsArray(std::array<std::string, size>& outArray) {}
+
+		template <uint32_t size, typename Head, typename... Tail>
+		static void _convertToStringsArray(std::array<std::string, size>& outArray, Head&& firstArg, Tail&&... rest)
+		{
+			outArray[size - 1 - sizeof...(Tail)] = ToString(std::forward<Head>(firstArg));
+			_convertToStringsArray<size, Tail...>(outArray, std::forward<Tail>(rest)...);
+		}
 	};
 }
