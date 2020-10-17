c6abfbc7fe1f743e0f08b822860e82195e25c30e
bnoazx005 <ildar2571@yandex.ru>
2020-10-17 15:37:38 +0400

    Fix building pipeline for Unix systems

--- CI/travis/install.sh
+++ CI/travis/install.sh
@@ -3,8 +3,8 @@
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 
-sudo apt-get install -qq g++-7
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
+sudo apt-get install -qq g++-9
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 90
 
 sudo apt-get install -qq unzip
 
